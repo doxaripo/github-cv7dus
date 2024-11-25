8ac2f782be2932ca433d41160732ea01f709dc90
doxaripo <accept9real99@outlook.jp>
2024-11-25 16:41:34 +0900

    06終了

--- src/App.svelte
+++ src/App.svelte
@@ -2,29 +2,37 @@
     import AnswerButton from "./AnswerButton.svelte";
     let currentScore = 0;
     let renzokuSeikai = 0;
-
-    function answerButtonClicked(){
+    let quizdata = {
+      mondai: "鮪", 
+      seikai: "まぐろ", 
+      taku: ["ひらめ", "かれい", "まぐろ", "ぶり", "くじら"]
+    };
+  
+    function answerButtonClicked(isCorrect){
+      if(isCorrect){
         renzokuSeikai += 1;
         currentScore += renzokuSeikai;
+      }
+      else{
+        renzokuSeikai = 0;
+      }
     }
-</script>
-
-<main class="flex flex-col h-svh">
+  </script>
+  
+  <main class="flex flex-col h-svh">
     <!-- メニューバー -->
     <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
-        <div>スコア:{currentScore}</div>
-        <div>連続正解数:{renzokuSeikai}</div>
+      <div>スコア:{currentScore}</div>
+      <div>連続正解数:{renzokuSeikai}</div>
     </div>
-  
+    
     <!-- 問題 -->
-    <div class="bg-green-200 text-center text-4xl py-4">鮪</div>
+    <div class="bg-green-200 text-center text-4xl py-4">{quizdata.mondai}</div>
     
     <!-- 選択肢 -->
     <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
-        <AnswerButton on:click={answerButtonClicked}>ひらめ</AnswerButton>
-        <AnswerButton on:click={answerButtonClicked}>かれい</AnswerButton>
-        <AnswerButton on:click={answerButtonClicked}>まぐろ</AnswerButton>
-        <AnswerButton on:click={answerButtonClicked}>ぶり</AnswerButton>
-        <AnswerButton on:click={answerButtonClicked}>くじら</AnswerButton>
+      {#each quizdata.taku as t}
+        <AnswerButton on:click={() => answerButtonClicked(quizdata.seikai===t)}>{t}</AnswerButton>
+      {/each}
     </div>
-</main>
+  </main>
